f0172a5a04507661e2558da2b6e1f49ee8885ce7
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-07-12 15:23:46 +0300

    feat: Add about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { marked } from 'marked';
+
+	let text = '';
+
+	async function getReadmeText() {
+		const res = await fetch(`https://cdn.jsdelivr.net/gh/basicpixel/marktic/README.md`);
+		text = await res.text();
+	}
+
+	getReadmeText();
+</script>
+
+<svelte:head>
+	<title>marktic: the dumbest markdown SSG</title>
+</svelte:head>
+
+<div class="max-w-full prose 2xl:prose-xl prose-invert" dir="auto">
+	{@html marked(text, { gfm: true })}
+</div>
